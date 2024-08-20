a8507ad80d68d9b59e75c0d87fff7f3498377127
mprojr <martinprojcs@gmail.com>
2024-08-20 17:27:35 -0500

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,12 +1,13 @@
 import Login from '@/components/Login';
 import Main from '@/components/Main';
+import Dashboard from '@/components/Dashboard';
 import React from 'react'
 
 export const metadata = {
   title: "Journal - Dashboard",
 };
 
-export default function Dashboard() {
+export default function DashboardPage() {
 
   const isAuthenticated = true
 
