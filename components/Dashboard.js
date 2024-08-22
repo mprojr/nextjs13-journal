5ed5ff078978fe599bd9c9989724ff4d77e331c7
mprojr <martinprojcs@gmail.com>
2024-08-22 16:24:33 -0500

    Dashboard upgrade

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Fugaz_One } from 'next/font/google'
+
+const fugaz = Fugaz_One({ subsets: ["latin"], weight: ['400'] })
 
 export default function Dashboard() {
+
+
+  const statuses = {
+    num_days: 14,
+    time_remaining: '13:14:26',
+    date: (new Date()).toDateString()
+  }
+
   return (
-    <div>
-      <p>Dashboard</p>
+    <div className='flex flex-col flex-1 '>
+      <div className='grid grid-cols-1 sm:grid-cols-3 bg-indigo-50 text-indigo-500 '>
+        {Object.keys(statuses).map((status, statusIndex) => {
+          return (
+          <div key={statusIndex} className='p-4'>
+            <p className='font-medium uppercase text-xs sm:text-sm'>{status.replaceAll('_', ' ')}</p>
+            <p className={'text-base sm:text-lg ' + fugaz.className }>{statuses[status]}</p>
+          </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
