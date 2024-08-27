372534af109c33e13e8d92f3f5c8e89041269ac6
mprojr <martinprojcs@gmail.com>
2024-08-27 14:58:43 -0500

    Journal Color Fixes.

--- components/Journal.js
+++ components/Journal.js
@@ -6,14 +6,14 @@ const fugaz = Fugaz_One({ subsets: ['latin'], weight: ['400'] });
 
 export default function JournalPage() {
   return (
-    <div className="relative min-h-screen flex flex-col items-center p-8 bg-white bg-opacity-75">
-      <div className="w-full min-w-[200px] max-w-3xl mx-auto sm:p-4 p-4 sm:bg-notebook sm-bg-notebook bg-no-repeat bg-cover rounded-xl shadow-lg overflow-hidden ">
-        <h2 className={'text-3xl font-extrabold text-center text-teal-coral-6 mb-6 ' + fugaz.className}>
+    <div className="relative min-h-screen flex flex-col items-center p-8 text-black bg-white bg-opacity-75">
+      <div className="w-full min-w-[250px] max-w-3xl sm:p-4 p-4 sm:bg:notebook sm-bg-notebook bg-no-repeat bg-cover  mx-auto bg-white bg-opacity-90 rounded-xl shadow-lg overflow-hidden ">
+        <h2 className={'text-3xl font-extrabold text-center text-teal-coral-6 mb-12 ' + fugaz.className}>
           Journal
         </h2>
         <textarea
+          className="w-full h-96 bg-bottom bg-notebook-lines border-none focus:outline-none resize-none text-slate-600 text-lg leading-relaxed"
           placeholder="Write your thoughts here..."
-          className="w-full h-96 p-6 bg-transparent border-none focus:outline-none resize-none text-teal-coral-6 text-lg leading-relaxed"
         />
       </div>
     </div>
